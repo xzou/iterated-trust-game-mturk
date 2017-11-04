aed7bc2f3d289da61708fa54b15a1469f36ff57c
Cindy Zou <xzou@email.wm.edu>
2017-11-03 23:32:54 -0400

    Change name service to IP service

--- src/app/ip.service.ts
+++ src/app/ip.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Jsonp } from '@angular/http';
 
 @Injectable()
-export class NameService {
+export class IpService {
 
   constructor(private jsonp: Jsonp) { }
 
